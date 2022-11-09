db03882141905019c183e926bb7020394718d5e4
Lu-syuan <as6552783@gmail.com>
2022-11-09 12:01:21 +0800

    NavSection Component

--- thef2eweek2/src/App.vue
+++ thef2eweek2/src/App.vue
@@ -1,32 +1,29 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <nav-section></nav-section>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+// import NavSection from './components/nav.vue'
 
-nav {
-  padding: 30px;
+// export default {
+//   components: {
+//     NavSection: NavSection
+//   }
+// }
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+import NavSection from './components/NavSection.vue'
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default {
+  components: {
+    NavSection: NavSection
   }
 }
+</script>
+
+<style scoped lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100;300;400;500;700;900&display=swap');
+@import './assets/main.scss';
+@import './assets/cssreset.css';
 </style>
---
+++ thef2eweek2/src/components/NavSection.vue
@@ -0,0 +1,52 @@
+<template>
+  <nav id="nav">
+    <div class="logo">
+      <img src="../assets/image/LOGO-2.png" alt="" />
+    </div>
+    <div class="navbar">
+      <ul>
+        <li>邀請他人簽屬</li>
+        <li>簽署新文件</li>
+        <li>登入</li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<style scoped lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100;300;400;500;700;900&display=swap');
+@import '../assets/main.scss';
+@import '../assets/cssreset.css';
+
+#nav {
+  background-color: $light-main;
+  width: 100%;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 17px 242px;
+  > .logo {
+    width: 100px;
+    height: 25px;
+  }
+  > .navbar {
+    > ul {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      > li {
+        display: inline-block;
+        color: $secondary;
+        letter-spacing: 0.2em;
+        @extend %fontFamily;
+        @extend %HeadLine3;
+        padding: 0 15px;
+      }
+      > li:nth-child(1) {
+        color: #e2e1dd;
+      }
+    }
+  }
+}
+</style>
